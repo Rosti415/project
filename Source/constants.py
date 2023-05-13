103ae6eca3bc97b78887e2317258605e463e80d0
Rosti415 <116059105+Rosti415@users.noreply.github.com>
2023-05-13 16:58:06 +0300

    pin

--- Source/constants.py
+++ Source/constants.py
@@ -6,6 +6,8 @@ FPS = 60
 START_X, START_Y = 20, 400
 WALL_X,WALL_Y = 100,50
 WALL_WIDTH,WALL_HEIGHT = 100,50
+PIN_X,PIN_Y = 75,100
+PIN_WIDTH,PIN_HEIGHT= 100,75
 
 BACKGROUND_IMAGE = pygame.transform.scale(
     pygame.image.load(r'C:\Users\User\Documents\GitHub\project\Assets\sky_mario.jpeg.'), (WIDTH, HEIGHT))
@@ -13,3 +15,5 @@ WALL_IMAGE = pygame.transform.scale(
     pygame.image.load(r"C:\Users\User\Documents\GitHub\project\Assets\wall.png"),(WALL_WIDTH,WALL_HEIGHT))
 MARIO_IMAGE = pygame.transform.scale(
     pygame.image.load(r"C:\Users\User\Documents\GitHub\project\Assets\mario.png"), (MARIO_WIDTH, MARIO_HEIGHT))
+PIN_IMAGE = pygame.transform.scale(
+    pygame.image.load(r"C:\Users\User\Documents\GitHub\project\Assets\pin.png"), (PIN_WIDTH, PIN_HEIGHT))
--- Source/wall.py
+++ Source/wall.py
@@ -4,3 +4,7 @@ from Source.tiles import Tiles
 class Wall(Tiles):
     def __init__(self, size, start_coordinates, image):
         super().__init__(size, start_coordinates, image)
+
+class PIN(Tiles):
+    def __init__(self, size, start_coordinates, image):
+        super().__init__(size, start_coordinates, image)
